a639ded09f7b4e084547786f1ca9778c0df41575
ezolenko <zolenkoe@gmail.com>
2017-05-29 00:20:04 -0600

    - fix for java filenames

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -58,8 +58,8 @@ cd /tmp
 wget -nv --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
 
 mkdir /opt/jdk
-tar -zxf jdk-8u92-linux-x64.tar.gz -C /opt/jdk
-rm jdk-8u92-linux-x64.tar.gz
+tar -zxf jdk-8u131-linux-x64.tar.gz -C /opt/jdk
+rm jdk-8u131-linux-x64.tar.gz
 update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_131/bin/java 100
 update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_131/bin/javac 100
 cd $DIR
